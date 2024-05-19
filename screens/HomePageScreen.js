c7b2d85ff9ee42c7d03d1233793a93d8051a6365
Gulbeyaz <glbzbayram@gmail.com>
2024-05-19 10:48:51 +0300

    screens were navigated

--- screens/HomePageScreen.js
+++ screens/HomePageScreen.js
@@ -7,23 +7,59 @@ import {
 } from "react-native";
 import React, { useContext } from "react";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 export default function HomePageScreen({ navigation }) {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, removeBlogPost } = useContext(Context);
 
   return (
     <View>
-      <TouchableOpacity onPress={addBlogPost}>
-        <Text>Ekle</Text>
-      </TouchableOpacity>
+      {/* <TouchableOpacity style={styles.addBtn} onPress={addBlogPost}>
+        <Text style={styles.addText}>Ekle</Text>
+      </TouchableOpacity> */}
       <FlatList
         data={state}
+        keyExtractor={(state) => state.id}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <TouchableOpacity
+              style={styles.row}
+              onPress={() => navigation.navigate("ShowScreen", { id: item.id })}
+            >
+              <View style={styles.contentColumns}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.title}>{item.content}</Text>
+              </View>
+              <TouchableOpacity onPress={() => removeBlogPost(item.id)}>
+                <Feather name="trash" size={24} color="black" />
+              </TouchableOpacity>
+            </TouchableOpacity>
+          );
         }}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  contentColumns: {
+    flexDirection: "column",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+  },
+  title: {
+    fontSize: 18,
+  },
+  addBtn: {
+    backgroundColor: "blue",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderColor: "wight",
+    borderRadius: 15,
+  },
+});
