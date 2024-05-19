c44fad0e7d3280d0a4d49047fc6ca960b58eb757
Gulbeyaz <glbzbayram@gmail.com>
2024-05-19 10:46:53 +0300

    createScreen and Edit screen were created to creating blog posts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { Image, StyleSheet, Platform, TouchableOpacity } from "react-native";
 import CreateScreen from "../../screens/CreateScreen";
 import HomePageScreen from "../../screens/HomePageScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Provider } from "../../context/BlogContext";
+import ShowScreen from "../../screens/ShowScreen";
+import EditScreen from "../../screens/EditScreen";
+import { AntDesign } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +15,34 @@ export default function HomeScreen() {
     <Provider>
       <NavigationContainer independent={true}>
         <Stack.Navigator screenOptions={{ headerTitle: "Benim Blogum" }}>
-          <Stack.Screen name="Home" component={HomePageScreen} />
+          <Stack.Screen
+            name="Home"
+            component={HomePageScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+                  <AntDesign name="plus" size={24} color="black" />
+                </TouchableOpacity>
+              ),
+            })}
+          />
           <Stack.Screen name="Create" component={CreateScreen} />
+          <Stack.Screen name="Edit" component={EditScreen} />
+          <Stack.Screen
+            name="ShowScreen"
+            component={ShowScreen}
+            options={({ navigation, route }) => ({
+              headerRight: () => (
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate("Edit", { id: route.params })
+                  }
+                >
+                  <AntDesign name="edit" size={24} color="black" />
+                </TouchableOpacity>
+              ),
+            })}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
